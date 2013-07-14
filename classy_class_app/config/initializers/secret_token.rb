cd1ab74c6b3dc76de3480ae5d151ef8328734ec9
Werdyl <eddywerdy@gmail.com>
2013-07-14 13:19:30 -0400

    Added basic Rails structure and Schedule model + controller

---
+++ classy_class_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ClassyClassApp::Application.config.secret_token = 'd1f482823e244b6d12b6c39cb3c9fe9eb8c53c3a29e2debf195c759d75ccd1d7d303230622e68358ffbcc9be45345077232b1a2fe5b9b543ecbc50a613b70185'
