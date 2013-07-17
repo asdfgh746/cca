d5d1e021a6d871b6d8e95b31a32c941960949270
Edward Lu <eddywerdy@gmail.com>
2013-07-17 12:53:47 -0400

    added signup functionality

--- classy_class_app/config/routes.rb
+++ classy_class_app/config/routes.rb
@@ -49,7 +49,11 @@ ClassyClassApp::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   
+  resources :users
+
   root :to => 'static_pages#home'
+
+  match '/signup', to: 'users#new', via: 'get'
 
   # See how all your routes lay out with "rake routes"
 
