21f42b014b689056e082103c9e91a434d8013fda
Edward Lu <eddywerdy@gmail.com>
2013-07-17 18:34:48 -0400

    added forms for new schedule and class

--- classy_class_app/app/controllers/classy_classes_controller.rb
+++ classy_class_app/app/controllers/classy_classes_controller.rb
@@ -1,2 +1,14 @@
 class ClassyClassesController < ApplicationController
+
+	def new
+		render 'new'
+	end
+
+	def create
+		@classy = ClassyClass.new(params[:classy_class])
+		@classy.save
+		flash[:success] = "Schedule updated!"
+		redirect_to :back
+	end
+
 end
--- classy_class_app/app/controllers/schedules_controller.rb
+++ classy_class_app/app/controllers/schedules_controller.rb
@@ -1,4 +1,13 @@
 class SchedulesController < ApplicationController
 	def show
 	end
+
+	def create
+		@schedule = Schedule.new(params[:schedule])
+		@schedule.belongs_to_user = current_user.id
+		@schedule.save(validate: false)
+
+		flash[:success] = "Schedule created"
+		redirect_to :back
+	end
 end
