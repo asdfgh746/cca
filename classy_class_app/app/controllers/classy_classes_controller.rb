cbc9b2988697fd5effc4c05b57e09fad98feccfc
Edward Lu <eddywerdy@gmail.com>
2013-08-20 18:20:19 -0400

    more things with blocks

--- classy_class_app/app/controllers/classy_classes_controller.rb
+++ classy_class_app/app/controllers/classy_classes_controller.rb
@@ -7,6 +7,8 @@ class ClassyClassesController < ApplicationController
 	def create
 		@classy = ClassyClass.create(params[:classy_class])
 
+		
+
 		flash[:success] = "Schedule updated!"
 		redirect_to root_path
 	end
