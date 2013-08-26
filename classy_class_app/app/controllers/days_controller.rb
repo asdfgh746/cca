80c26c3fbbc684bc4e6b168608edb376747cf661
werdyl <eddywerdy@gmail.com>
2013-08-26 14:17:05 -0400

    added ability to change order of blocks for each day

--- classy_class_app/app/controllers/days_controller.rb
+++ classy_class_app/app/controllers/days_controller.rb
@@ -5,4 +5,11 @@ class DaysController < ApplicationController
 
 	def edit
 	end
+
+	def update
+		@day = Day.find_by_id(params[:id])
+		@day.update_attributes(params[:day])
+		flash[:success] = "Profile updated."
+		redirect_to :back
+	end
 end
